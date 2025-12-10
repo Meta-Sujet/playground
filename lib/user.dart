9284a420599900715398e93a3de2728aad5333ac
sujet <meta.sujet@gmail.com>
2025-12-10 16:08:48 +0400

    id added to user

--- lib/user.dart
+++ lib/user.dart
@@ -1,4 +1,10 @@
 class User {
-  User({required this.name});
+  User({required this.name,required this.id});
   final String name;
+  final int id;
+
+  @override
+  String toString() {
+    return "$name id is: $id";
+  }
 }
